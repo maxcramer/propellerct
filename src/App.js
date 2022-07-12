0e543e63a57fa89674f25226730f87bf97c507f5
maxcramer <maximiliancramer@yahoo.com>
2022-07-12 15:01:18 +0100

    adding some basic styling, to break down into components next

--- src/App.js
+++ src/App.js
@@ -16,17 +16,18 @@ function App() {
     }
   }, []);
 
-  console.log(images);
-
   if (!images) return <div>No Images Avaliable</div>;
+
   return (
     <div className="App">
       {images.map((item) => (
         <div key={item.data[0].nasa_id} className="item_container">
-          <h3>{item.data[0].title}</h3>
-          <img src={item.links[0].href} alt="" />
-          <p>{item.data[0].date_created.split("T")[0]}</p>
-          <p>{item.data[0].description}</p>
+          <h3 className="title">{item.data[0].title}</h3>
+          <div className="data_container">
+            <img src={item.links[0].href} alt="" />
+            <p className="date">{item.data[0].date_created.split("T")[0]}</p>
+            <p className="desc">{item.data[0].description}</p>
+          </div>
         </div>
       ))}
     </div>
