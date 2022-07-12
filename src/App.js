97694f72da7d021f1ab8afdbf8669f25ca468883
maxcramer <maximiliancramer@yahoo.com>
2022-07-12 13:32:13 +0100

    looping over the items

--- src/App.js
+++ src/App.js
@@ -8,17 +8,25 @@ function App() {
     fetchData();
     async function fetchData() {
       const res = await fetch(
-        `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=4r7rlpLmJt48cg4tmTUYangEZIQUqE1kwAiPPOcS`
+        `https://images-api.nasa.gov/search?media_type=image`
       );
-      console.log(res);
       const data = await res.json();
-      setImages(data);
+      const items = await data.collection.items;
+      console.log(items);
+      setImages(items);
     }
   }, []);
   if (!images) return <div>No Images Avaliable</div>;
-  console.log(images);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {images.map((item) => (
+        <div key={item.href}>
+          <h2>Testing</h2>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
