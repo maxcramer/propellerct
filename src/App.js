d049c7d85cb9a497699332cfa25241984c189211
maxcramer <maximiliancramer@yahoo.com>
2022-07-12 14:12:25 +0100

    rendering images from nasa api, grid style layout

--- src/App.js
+++ src/App.js
@@ -12,17 +12,16 @@ function App() {
       );
       const data = await res.json();
       const items = await data.collection.items;
-      console.log(items);
       setImages(items);
     }
   }, []);
-  if (!images) return <div>No Images Avaliable</div>;
 
+  if (!images) return <div>No Images Avaliable</div>;
   return (
     <div className="App">
       {images.map((item) => (
-        <div key={item.href}>
-          <h2>Testing</h2>
+        <div key={item.data[0].nasa_id} className="item_container">
+          <img src={item.links[0].href} alt="" />
         </div>
       ))}
     </div>
