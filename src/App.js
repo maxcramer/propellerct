f0982a2f44c8648b7d7105e71cdf85ae631b1ebc
maxcramer <maximiliancramer@yahoo.com>
2022-07-12 13:08:38 +0100

    console logging images array from api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>TESTING</h1>
-    </div>
-  );
+  const [images, setImages] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+    async function fetchData() {
+      const res = await fetch(
+        `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=4r7rlpLmJt48cg4tmTUYangEZIQUqE1kwAiPPOcS`
+      );
+      console.log(res);
+      const data = await res.json();
+      setImages(data);
+    }
+  }, []);
+  if (!images) return <div>No Images Avaliable</div>;
+  console.log(images);
+
+  return <div className="App"></div>;
 }
 
 export default App;
