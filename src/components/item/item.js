52c3b07f1a4279ada7043469eab0acf4e8678308
maxcramer <maximiliancramer@yahoo.com>
2022-07-13 11:46:16 +0100

    popup showing on item in list click, to pass the data in next and get it to close

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Popup from "../popup/popup";
 
 import "./item.css";
 
-const item = ({ item }) => {
+function Item({ item }) {
+  const [popup, setPopup] = useState(false);
+
   return (
-    <div>
+    <div onClick={() => setPopup(true)}>
       <h3 className="title">{item.data[0].title}</h3>
       <div className="data_container">
         <img src={item.links[0].href} alt="" />
         <p className="date">{item.data[0].date_created.split("T")[0]}</p>
         <p className="desc">{item.data[0].description}</p>
       </div>
+      <Popup item={item} trigger={popup}>
+        My Popup
+      </Popup>
     </div>
   );
-};
+}
 
-export default item;
+export default Item;
---
+++ src/components/popup/popup.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./popup.css";
+
+const popup = (props) => {
+  return props.trigger ? (
+    <div className="popup">
+      <div className="popup-inner">
+        <h3>Popup</h3>
+        {props.children}
+        <button className="close">X</button>
+      </div>
+    </div>
+  ) : (
+    ""
+  );
+};
+
+export default popup;
