4b07546223f1004d4ac41a43bdc8cc057b693926
maxcramer <maximiliancramer@yahoo.com>
2022-07-13 11:53:48 +0100

    close function working onpopup, to do some styling next

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -8,12 +8,14 @@ function Item({ item }) {
   const [popup, setPopup] = useState(false);
 
   return (
-    <div onClick={() => setPopup(true)}>
-      <h3 className="title">{item.data[0].title}</h3>
-      <div className="data_container">
-        <img src={item.links[0].href} alt="" />
-        <p className="date">{item.data[0].date_created.split("T")[0]}</p>
-        <p className="desc">{item.data[0].description}</p>
+    <div>
+      <div onClick={() => setPopup(true)}>
+        <h3 className="title">{item.data[0].title}</h3>
+        <div className="data_container">
+          <img src={item.links[0].href} alt="" />
+          <p className="date">{item.data[0].date_created.split("T")[0]}</p>
+          <p className="desc">{item.data[0].description}</p>
+        </div>
       </div>
       <Popup item={item} trigger={popup} setTrigger={setPopup}>
         <h3 className="title">{item.data[0].title}</h3>
